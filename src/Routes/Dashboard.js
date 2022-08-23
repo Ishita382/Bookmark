710e75637c2aedf4c5dc2d84f4d0767d995bf04d
Ishita <ishitaarora382@gmail.com>
2022-08-23 16:17:42 +0530

    modified:   src/Routes/Dashboard.js 	modified:   src/App.js 	modified:   src/Redux/Actions/actions.js 	modified:   src/Redux/Actions/constant.js 	modified:   src/Redux/Sagas/index.js 	modified:   src/Redux/Sagas/register.js 	modified:   src/Routes/SignUp.js 	src/Redux/Sagas/getMyFolders.js

--- src/Routes/Dashboard.js
+++ src/Routes/Dashboard.js
@@ -2,11 +2,14 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getMe } from "../Redux/Actions/actions";
+import { getMyFolders } from "../Redux/Actions/actions";
+
 function Dashboard() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getMe());
+    dispatch(getMyFolders());
   });
   return (
     <div>
@@ -14,6 +17,8 @@ function Dashboard() {
       <h2>Quick Link</h2>
       <h3>URL</h3>
       <input type="text" placeholder="paste your link here" />
+      <h3>Folder</h3>
+      <input type="text"/>
       <button>Save</button>
     </div>
   );
