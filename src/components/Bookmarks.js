dbfa7a5ae028920c3452576bdc180374527ee6f8
Ishita <ishitaarora382@gmail.com>
2022-09-02 17:16:24 +0530

    modified:   src/components/Bookmarks.js

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -3,15 +3,10 @@ import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 
 const CustomBox = styled(Box)`
-flex: 1;
-
-`
-function Bookmark(){
-    return (
-        <CustomBox>
-           
-        </CustomBox>
-    )
+  flex: 1;
+`;
+function Bookmark() {
+  return <CustomBox></CustomBox>;
 }
 
 export default Bookmark;
