0be3094f799996d11288d73d40cf8ad0b0c689db
Ishita <ishitaarora382@gmail.com>
2022-09-08 13:14:20 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
-
+import styled from "@emotion/styled";
 import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import SignUp from "./routes/SignUp";
 import Login from "./routes/Login";
 import Dashboard from "./routes/Dashboard";
 
+const AppContainer = styled(Container)`
+  display: flex;
+ 
+`;
+
+const RouteBox = styled(Box)`
+  flex: 2;
+`;
 function App() {
   return (
     <Router>
-      <Container
-        sx={{
-          display: "flex",
-
-          flexDirection: "row",
-        }}
-      >
-        <Box
-          sx={{
-            flex: 2,
-          }}
-        >
+      <AppContainer>
+        <RouteBox>
           <Routes>
             <Route exact path="/" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
-        </Box>
-      </Container>
+        </RouteBox>
+      </AppContainer>
     </Router>
   );
 }
